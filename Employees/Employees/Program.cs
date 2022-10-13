c4bbdd6c86512cf3231ff136b8559ebfb796f535
Rushikesh041 <jadhavrushi041@gmail.com>
2022-10-13 13:27:08 +0530

    Upload UC3

--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -4,16 +4,20 @@
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int IS_PART_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
             int emphrs = 0;
             int empwage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0,2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0,3);
+            if (empCheck == IS_PART_TIME)
+            {
+                emphrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME ) 
             {
                 emphrs = 8;
-
             }
             else
             {
