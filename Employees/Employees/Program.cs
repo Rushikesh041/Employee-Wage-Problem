7375efdadbd1be359fc1a062f5cc1f31b07756ef
Rushikesh041 <jadhavrushi041@gmail.com>
2022-10-13 12:57:45 +0530

    UC1

--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
-            Random random = new Random();
-            int empCheck = random.Next(0,2);
+            Random ran = new Random();
+            int empCheck = ran.Next(0,2);
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
