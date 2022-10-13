5ca55fb3cf726e745b664e783e5d06efaf4c782b
Rushikesh041 <jadhavrushi041@gmail.com>
2022-10-13 13:14:49 +0530

    Upload UC

--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -5,17 +5,22 @@
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
-            Random ran = new Random();
-            int empCheck = ran.Next(0,2);
+            int EMP_RATE_PER_HOUR = 20;
+            int emphrs = 0;
+            int empwage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0,2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                emphrs = 8;
 
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                emphrs = 0;
             }
+            empwage = emphrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("EMP WAGE : "+empwage);
         }
     }
 }
