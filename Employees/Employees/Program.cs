13bfa04a0a41821e5b0d0b8b4dbbdd8c253bad36
Rushikesh041 <jadhavrushi041@gmail.com>
2022-10-19 18:33:26 +0530

    Upload UC3PartTime

--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -4,12 +4,15 @@
     {
         static void Main(string[] args)
         {
+            //Constants
             int IS_FULL_TIME = 2;
             int IS_PART_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
+            //Variables
             int emphrs = 0;
             int empwage = 0;
             Random random = new Random();
+            //Calculations
             int empCheck = random.Next(0,3);
             if (empCheck == IS_PART_TIME)
             {
