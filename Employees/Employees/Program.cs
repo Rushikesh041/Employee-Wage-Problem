2e9ff0cbfd82f7c80b8d10d73a0dbb76a5357d51
Rushikesh041 <jadhavrushi041@gmail.com>
2022-10-19 18:29:46 +0530

    Upload UC2DailyWage

--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -4,11 +4,14 @@
     {
         static void Main(string[] args)
         {
+            //Constants
             int IS_FULL_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
+            //Variables
             int emphrs = 0;
             int empwage = 0;
             Random random = new Random();
+            //Calculations
             int empCheck = random.Next(0,2);
             if (empCheck == IS_FULL_TIME)
             {
