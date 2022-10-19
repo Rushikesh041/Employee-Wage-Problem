4aabcf70f3baa1c5ec418bb6ffdfa05a89e4b83c
Rushikesh041 <jadhavrushi041@gmail.com>
2022-10-19 19:38:22 +0530

    Upload UC9ManageWageMultiple

--- Employees/Employees/CompanyEmpWage.cs
+++ Employees/Employees/CompanyEmpWage.cs
@@ -6,31 +6,57 @@ using System.Threading.Tasks;
 
 namespace Employees
 {
-    public class CompanyEmpWage
-    {
-        public string company;
-        public int empRatePerHour;
-        public int numOfWorkingDays;
-        public int maxHoursPerMonth;
-        public int totalEmpWage;
-
-        public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        public class EmpWageBuilderObject
         {
-            this.company = company;
-            this.empRatePerHour = empRatePerHour;
-            this.numOfWorkingDays = numOfWorkingDays;
-            this.maxHoursPerMonth = maxHoursPerMonth;
-        }
+            public const int IS_FULL_TIME = 1, IS_PART_TIME = 2;
+            private string company;
+            private int empRatePerHour;
+            private int numOfWorkingDays;
+            private int maxHoursPerMonth;
+            private int totalEmpWage;
 
-        public void setTotalEmpWage(int totalEmpWage)
-        {
-            this.totalEmpWage = totalEmpWage;
-        }
+            public EmpWageBuilderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+            {
+                this.company = company;
+                this.empRatePerHour = empRatePerHour;
+                this.numOfWorkingDays = numOfWorkingDays;
+                this.maxHoursPerMonth = maxHoursPerMonth;
+            }
 
-        public string toString()
-        {
-            return "Total Emp Wage of " + this.company + " is " + this.totalEmpWage;
+            public void computeEmpWage()
+            {
+
+                int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+                while (totalEmpHrs <= this.maxHoursPerMonth && totalWorkingDays < this.numOfWorkingDays)
+                {
+                    totalWorkingDays++;
+                    Random random = new Random();
+                    int empCheck = random.Next(0, 3);
+                    switch (empCheck)
+                    {
+                        case IS_FULL_TIME:
+                            empHrs = 8;
+                            break;
+                        case IS_PART_TIME:
+                            empHrs = 4;
+                            break;
+                        default:
+                            empHrs = 0;
+                            break;
+                    }
+                    totalEmpHrs += empHrs;
+                    Console.WriteLine("Days : " + totalWorkingDays + " Emp Hrs : " + empHrs);
+                }
+
+                totalEmpWage = totalEmpHrs * empRatePerHour;
+                Console.WriteLine("Total Emp Wage of " + company + " is :" + totalEmpWage);
+            }
+
+            public string toString()
+            {
+                return "Total Emp Wage of " + this.company + " is " + this.totalEmpWage;
+            }
         }
-    }
 }
 
--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -4,18 +4,12 @@
     {
         static void Main(string[] args)
         {
-            EmpWageBuilderObject Airtel = new EmpWageBuilderObject("Airtel", 30, 30, 140);
-            EmpWageBuilderObject Jio = new EmpWageBuilderObject("Jio", 40, 28, 130);
-            EmpWageBuilderObject VI = new EmpWageBuilderObject("VI", 25, 30, 120);
-            EmpWageBuilderObject BSNL = new EmpWageBuilderObject("BSNL", 30, 25, 100);
-            Airtel.computeEmpWage();
-            Console.WriteLine(Airtel.toString());
-            Jio.computeEmpWage();
-            Console.WriteLine(Jio.toString());
-            VI.computeEmpWage();
-            Console.WriteLine(VI.toString());
-            BSNL.computeEmpWage();
-            Console.WriteLine(BSNL.toString());
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addcompanyEmpWage("Airtel", 30, 30, 140);
+            empWageBuilder.addcompanyEmpWage("Jio", 40, 28,130);
+            empWageBuilder.addcompanyEmpWage("VI", 25, 30, 120);
+            empWageBuilder.addcompanyEmpWage("BSNL", 30, 25, 100);
+            empWageBuilder.computeEmpWage();
 
         }
     }
